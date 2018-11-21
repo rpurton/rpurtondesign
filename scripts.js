25b3c02ae27b4290d41d09f26f863a783c4ed5c0
rpurton <rpurton87@gmail.com>
2018-11-20 17:05:37 -0800

    fixes to header, animation background, hover functionality for tiles

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-console.log('stuff');
+/*
+function draw() {
+        var canvas = document.getElementById('animation');
+        if (canvas.getContext) {
+          var ctx = canvas.getContext('2d');
+            ctx.strokeStyle = '#4e4c50';
+            ctx.strokeRect(10, 10, 50, 50);
 
-// document.write('other stuff');
+            ctx.strokeRect(30, 30, 50, 50);
+
+            ctx.beginPath();
+            ctx.moveTo(75, 50);
+            ctx.lineTo(100, 75);
+            ctx.lineTo(100, 25);
+            ctx.lineTo(75, 50);
+            ctx.stroke();
+        }
+      }
+*/
